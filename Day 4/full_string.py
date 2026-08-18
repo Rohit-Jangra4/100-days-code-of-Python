3b8877da2f5c363555ee4147efbdbb6063b46e8e
Rohit <rjangra00528@gmail.com>
2026-08-18 22:14:45 +0530

    Added some changes

---
+++ Day 4/full_string.py
@@ -0,0 +1,41 @@
+# Full String
+
+# Strings ??
+
+str="Hello, World!"
+print(str)
+
+# Multi-line Strings
+
+str1='''This is multiline comment
+my name is the jack
+i am a good boy'''
+
+name="Rohit Jangra"
+
+print("Full Name:","Hello"+name)
+
+# Slicing of string
+
+str2="Cricket is nationl game of India. in this game 11 players are in playing"
+
+print(len(str2))
+print(str2[1:11])
+print(str2[1:])
+print(str2[:])
+print(str2[:25])
+print(str2[-23:-1])
+
+# Method of string
+
+a="python is developed by Gudio van Rossum.It is very fantastic and easy language."
+
+print(a.capitalize())
+print(a.center(50))
+print(a.count("is"))
+print(a.endswith("age."))
+print(a.startswith("python"))
+print(a.index('van'))
+print(a.isalnum())
+print(a.isalpha())
+print(a.replace("python","Java"))
---
+++ Day 4/if_esle_practice.py
@@ -0,0 +1,63 @@
+# if elif else practice
+
+# Question 1
+integer=int(input("Enter the number:"))
+if integer>0:
+    print("+ve number")
+elif integer==0:
+    print("Zero number")
+else:
+    print("-ve number")
+
+# Question 2
+
+marks=int(input("Marks:"))
+
+if marks>=90 and marks<100:
+    print("Grade: A")
+elif marks>=80 and marks<=89:
+    print("Grade: B")
+elif marks>=70 and marks<=79:
+    print("Grade: C")
+elif marks>=60 and marks<=69:
+    print("Grade: D")
+else:
+    print("Grade: E")
+
+# Question 3
+
+Age=int(input("Age:"))
+
+if Age<=13:
+    print("Child")
+elif Age>=14 and Age<=19:
+    print("Teenager")
+elif Age>=20 and Age<=59:
+    print('Adult')
+else:
+    print("Senior citizen")
+
+Question 4
+
+purchase_amnt=int(input("Amount:"))
+if purchase_amnt>=5000:
+    print("15% Discount and Total bill:", purchase_amnt-(15/100)*purchase_amnt)
+elif purchase_amnt>=3000 and purchase_amnt<=4999:
+    print("10% Discount and Total bill:",purchase_amnt-(10/100)*purchase_amnt)
+elif purchase_amnt>=2000 and purchase_amnt<=2999:
+    print("5% Discount and Total bill:",purchase_amnt-(5/100)*purchase_amnt)
+else:
+    print("No discount:",purchase_amnt)
+
+# Question 5
+
+a=int(input("a:"))
+b=int(input("b:"))
+c=int(input("c:"))
+
+if a>b and a>c:
+    print("a is greatest.")
+elif b>c:
+    print("b is greatest.")
+else:
+    print("c is greatest.")
