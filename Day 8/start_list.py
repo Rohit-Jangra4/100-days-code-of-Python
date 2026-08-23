daa0859c97c70143d9ca6ca3f08d953ec61a060a
Rohit <rjangra00528@gmail.com>
2026-08-23 22:02:25 +0530

    Day 8

---
+++ Day 8/start_list.py
@@ -0,0 +1,14 @@
+# Intro of List 
+
+lis=["Rohit","Murnal Thakur","Kangana Ranut"]
+print(type(lis))
+print(len(lis))
+print(lis)
+print(lis[0])
+print(lis[len(lis):-1])
+print(lis[1:5])
+print(lis[:])
+print(lis[-5:-1])
+
+list=[i*i for i in range(10) if i%2==0]
+print(list)
