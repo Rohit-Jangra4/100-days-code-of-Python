bfc7acfafd92996ed83f74547c8265344cc61618
Rohit <rjangra00528@gmail.com>
2026-08-24 21:37:43 +0530

    Add files via upload

---
+++ Day 9/tuple.py
@@ -0,0 +1,32 @@
+# Tuples
+
+# ---------------------------------------
+tup=(1,2,34,5,67,78)
+print(tup)
+print(type(tup))
+
+# --------------------------------------
+tuple=(1,2,3,4,5,6,7,89,56)
+print(tuple)
+print(tuple[2])
+print(tuple[2:6])
+print(tuple[:])
+print(tuple[:7])
+print(tuple[0:])
+print(tuple[-1:-4])
+print(tuple[:-1])
+print(tuple[-4:])
+
+#  check for item
+char=(1,3,5,6,4,64,3,8)
+if 7 in char:
+    print("Yes,present")
+else:
+    print("No,yet persent")
+
+# --------------------------------------
+items = ("Apple", "Mango", "Banana", "Orange", "Grapes")
+print(items)
+
+print(items.count("Apple"))
+
